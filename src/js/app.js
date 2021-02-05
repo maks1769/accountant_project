e8a5ddcabf369b5abfc4bc9c142d6b8aa4394362
maksim.o <maksim.o@mserverone.com>
2021-02-05 14:48:26 +0200

    project updtd

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,12 @@
-// import 'bootstrap';
-import {sum} from './components/utils'
-
-// import {devide} from './components/utils'
-
-// console.log((sum(0.1,0.2)));
-
-import customSelect from 'custom-select';
-// import Swiper, { Navigation, Pagination } from 'swiper';
-
-// Swiper.use([Navigation, Pagination]);
-
-customSelect('select');
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 10) {
+        $('.header').addClass('scroll-menu');
+    } else {
+        $('.header').removeClass('scroll-menu');
+    }
+});
 
 (function () {
-
     "use strict";
 
     let toggles = document.querySelectorAll(".wrap");
@@ -21,136 +14,60 @@ customSelect('select');
     for (let i = toggles.length - 1; i >= 0; i--) {
         let toggle = toggles[i];
         toggleHandler(toggle);
-    };
+    }
 
     function toggleHandler(toggle) {
         toggle.addEventListener("click", function (e) {
             e.preventDefault();
             this.classList.contains("is-active") === true ? this.classList.remove("is-active") : this.classList.add("is-active");
             document.querySelector('.nav').classList.contains("is-open") === true ? document.querySelector('.nav').classList.remove("is-open") : document.querySelector('.nav').classList.add("is-open");
+            document.querySelector('.header').classList.contains("is-open") === true ? document.querySelector('.header').classList.remove("is-open") : document.querySelector('.header').classList.add("is-open");
         });
     }
 })();
 
-// let mySwiper1 = undefined;
-//
-// function initSwiper() {
-//     let screenWidth = $(window).width();
-//     if (screenWidth > 1200 && mySwiper1 === undefined) {
-//         mySwiper1 = new Swiper('.first-slider', {
-//             grabCursor: true,
-//
-//             breakpoints: {
-//                 1260: {
-//                     slidesPerView: 4,
-//                     spaceBetween: 24,
-//                 },
-//                 1200: {
-//                     slidesPerView: 3,
-//                     spaceBetween: 24,
-//                 },
-//             },
-//
-//             navigation: {
-//                 nextEl: '.swiper-button-next',
-//                 prevEl: '.swiper-button-prev',
-//             },
-//         });
-//         $('.first-slider').addClass('swiper-container');
-//         $('.first-slider .catalog__list').addClass('swiper-wrapper');
-//         $('.first-slider .catalog__list product-card').addClass('swiper-slide');
-//     } else if (screenWidth < 1200 && mySwiper1 !== undefined) {
-//         mySwiper1.destroy();
-//         mySwiper1 = undefined;
-//         $('.first-slider').removeClass('swiper-container');
-//         $('.first-slider .catalog__list').removeClass('swiper-wrapper');
-//         $('.first-slider .catalog__list product-card').removeClass('swiper-slide');
-//     }
-// }
-//
-// //Swiper plugin initialization
-// initSwiper();
-//
-// //Swiper plugin initialization on window resize
-// $(window).on('resize', function(){
-//     initSwiper();
-// });
-//
-//
-// // slider 2
-// let mySwiper2 = undefined;
-//
-// function initSwiper2() {
-//     let screenWidth = $(window).width();
-//     if (screenWidth > 1200 && mySwiper2 === undefined) {
-//         mySwiper2 = new Swiper('.second-slider', {
-//             grabCursor: true,
-//
-//             breakpoints: {
-//                 1260: {
-//                     slidesPerView: 2,
-//                     spaceBetween: 10,
-//                 },
-//                 1200: {
-//                     slidesPerView: 3,
-//                     spaceBetween: 24,
-//                 },
-//             },
-//             pagination: {
-//                 el: '.swiper-pagination',
-//             },
-//         });
-//         $('.second-slider').addClass('swiper-container');
-//         $('.second-slider .catalog__list').addClass('swiper-wrapper');
-//         $('.second-slider .catalog__list product-card').addClass('swiper-slide');
-//     } else if (screenWidth < 1200 && mySwiper2 !== undefined) {
-//         mySwiper2.destroy();
-//         mySwiper2 = undefined;
-//         $('.second-slider').removeClass('swiper-container');
-//         $('.second-slider .catalog__list').removeClass('swiper-wrapper');
-//         $('.second-slider .catalog__list product-card').removeClass('swiper-slide');
-//     }
-// }
-//
-// //Swiper plugin initialization
-// initSwiper2();
-//
-// //Swiper plugin initialization on window resize
-// $(window).on('resize', function(){
-//     initSwiper2();
-// });
+jQuery(document).ready(function($) {
+    $(".label input").focus(function(){
+        // $(this).parent().removeClass("round");
+        $(this).parent().addClass("on-focus");
 
-$(document).ready(function(){
-    $('.slider-1').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: "unslick"
-            },
-        ]
-    });
-
-    $('.slider-2').slick({
-        infinite: true,
-        dots: true,
-        adaptiveHeight: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: "unslick"
-            },
-        ]
-    });
+    })
 });
 
+let lastId,
+    topMenu = $(".nav__list"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function() {
+        let item = $($(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
+    });
 
+menuItems.click(function(e) {
+    let href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 700);
+    e.preventDefault();
+});
 
+$(window).scroll(function() {
+    let fromTop = $(this).scrollTop() + topMenuHeight;
+
+    let cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    cur = cur[cur.length - 1];
+    let id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        menuItems
+            .parent().removeClass("active-scroll")
+            .end().filter("[href='#" + id + "']").parent().addClass("active-scroll");
+    }
+});
